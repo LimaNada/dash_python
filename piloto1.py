8eac19586aca668d938e45353b0182197bf2719b
LimaNada <gabriel_llimasilva@hotmail.com>
2023-09-27 20:09:04 -0300

    grafico_mensal

--- piloto1.py
+++ piloto1.py
@@ -6,9 +6,11 @@ import requests
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout='wide')
+
 
 def formata_numero(valor, prefixo=''):
-    for unidade in ['','mil']:
+    for unidade in ['', 'mil']:
         if valor < 1000:
             return f'{prefixo} {valor:.2f} {unidade}'
         valor /= 1000
@@ -19,15 +21,19 @@ st.title('Piloto de Streamlit 🛒')
 
 url = 'https://labdados.com/produtos'
 response = requests.get(url).json()
-
 dados = pd.DataFrame(response)
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
 
 # Tabelas
 receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset=['Local da compra'])[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mês'] = receita_mensal['Data da Compra'].dt.month_name()
+
 # Gráficos
-fig_mapa_receita = px.scatter_geo(receita_estados, 
+fig_mapa_receita = px.scatter_geo(receita_estados,
                                   lat='lat',
                                   lon='lon',
                                   scope='south america',
@@ -38,14 +44,26 @@ fig_mapa_receita = px.scatter_geo(receita_estados,
                                   title='Receita por Estado'
                                   )
 
+fig_receita_mensal = px.line(receita_mensal,
+                             x='Mês',
+                             y='Preço',
+                             markers=True,
+                             range_y=[0, receita_mensal.max()],
+                             color='Ano',
+                             line_dash='Ano',
+                             title='Receita Mensal'
+                             )
+
+fig_mapa_receita.update_layout(yaxis_title='Receita',)
 
 # Visualização no streamlit
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric("Receita", formata_numero(dados['Preço'].sum(), "R$"))
-    st.plotly_chart(fig_mapa_receita)
+    st.plotly_chart(fig_mapa_receita, use_container_width=True)
 
 with coluna2:
     st.metric("Quantidade de Vendas", formata_numero(dados.shape[0]))
+    st.plotly_chart(fig_receita_mensal, use_container_width=True)
 
 st.dataframe(dados)
